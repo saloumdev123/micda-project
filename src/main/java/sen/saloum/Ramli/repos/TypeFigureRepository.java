ee3e1bd61562bd702f38adc14407a267a5a25739
papa saloum sene <sensaloumdev@gmail.com>
2025-05-07 18:56:28 -0400

    adding type fidure repos

---
+++ src/main/java/sen/saloum/Ramli/repos/TypeFigureRepository.java
@@ -0,0 +1,7 @@
+package sen.saloum.Ramli.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sen.saloum.Ramli.enums.TypeFigure;
+
+public interface TypeFigureRepository extends JpaRepository<TypeFigure, String> {
+}
