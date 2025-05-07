fb4e8da00b19aa2d4c6b81b3e60624a75fcd8010
papa saloum sene <sensaloumdev@gmail.com>
2025-05-07 18:57:31 -0400

    adding user apper

---
+++ src/main/java/sen/saloum/Ramli/mapStruct/UtilisateurMapper.java
@@ -0,0 +1,16 @@
+package sen.saloum.Ramli.mapStruct;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import sen.saloum.Ramli.dto.UtilisateurDto;
+import sen.saloum.Ramli.models.Utilisateur;
+
+@Mapper(componentModel = "spring")
+public interface UtilisateurMapper {
+
+    UtilisateurMapper INSTANCE = Mappers.getMapper(UtilisateurMapper.class);
+
+    UtilisateurDto toDto(Utilisateur utilisateur);
+
+    Utilisateur toEntity(UtilisateurDto utilisateurDto);
+}
