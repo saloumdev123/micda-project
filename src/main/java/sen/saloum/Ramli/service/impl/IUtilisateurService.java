262e3aa3f0ab5fa80d136f25363119ad3bd13fa8
papa saloum sene <sensaloumdev@gmail.com>
2025-05-09 12:32:14 -0400

    CREATIGN interface Utilisateur for making the services reusble

---
+++ src/main/java/sen/saloum/Ramli/service/impl/IUtilisateurService.java
@@ -0,0 +1,13 @@
+package sen.saloum.Ramli.service.impl;
+
+import sen.saloum.Ramli.dto.UtilisateurDto;
+
+import java.util.List;
+
+public interface IUtilisateurService {
+    UtilisateurDto create(UtilisateurDto dto);
+    List<UtilisateurDto> getAll();
+    UtilisateurDto getById(Long id);
+    UtilisateurDto update(Long id, UtilisateurDto dto);
+    void delete(Long id);
+}
