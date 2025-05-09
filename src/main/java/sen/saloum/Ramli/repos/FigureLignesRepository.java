41cbbc2264735564ddd3045be82e3dfaefffe05f
papa saloum sene <sensaloumdev@gmail.com>
2025-05-09 09:28:45 -0400

    updating figure lign service

---
+++ src/main/java/sen/saloum/Ramli/repos/FigureLignesRepository.java
@@ -0,0 +1,7 @@
+package sen.saloum.Ramli.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sen.saloum.Ramli.models.FigureLigne;
+
+public interface FigureLignesRepository extends JpaRepository<FigureLigne, Long> {
+}
