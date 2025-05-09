81e221deadd0140629a6d8b57493bc62e392c421
papa saloum sene <sensaloumdev@gmail.com>
2025-05-09 09:25:57 -0400

    deleting type figure repos, and maper

--- src/main/java/sen/saloum/Ramli/repos/TypeFigureRepository.java
+++
@@ -1,7 +0,0 @@
-package sen.saloum.Ramli.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sen.saloum.Ramli.enums.TypeFigure;
-
-public interface TypeFigureRepository extends JpaRepository<TypeFigure, String> {
-}
